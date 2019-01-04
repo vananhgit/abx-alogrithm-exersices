93752abbeba7c52dcfbd3b959a5bc132552a0f7a
vananh <truongvananh2121997@gmail.com>
2019-01-04 12:09:12 +0700

    xoa code

--- src/reverse_string.java
+++ src/reverse_string.java
@@ -1,13 +1,8 @@
 // https://leetcode.com/problems/reverse-string/
 class Solution {
     public String reverseString(String s) {
-         String string="A man, a plan, a canal: Panama";
       String reverse = new StringBuffer(s).
       reverse().toString();
-      System.out.println("String before reverse:      "+string);
-      System.out.println("String after reverse:      "+reverse);
-    return reverse;
-   }
+      return reverse;
     }
-
-
+  }
